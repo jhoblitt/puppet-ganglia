975840e3f2df13791e9f27819abc7e59a0a81d75
Joshua Hoblitt <jhoblitt@cpan.org>
2012-11-13 17:04:33 -0700

    check for $::osfamily and fail() on unsupported platforms

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,25 +5,34 @@ class ganglia::params {
   $gmetad_package_name   = 'ganglia-gmetad'
   $gmetad_service_name   = 'gmetad'
 
-  case $lsbmajdistrelease {
-    # the epel packages change uid/gids + install paths between 5 & 6
-    5: {
-      $ganglia_uid = 298
-      $ganglia_gid = 298
-      $gmond_service_config = '/etc/gmond.conf'
-      $gmond_service_erb    = 'ganglia/gmond.conf.el5.erb'
+  case $::osfamily {
+    redhat: {
+      case $::lsbmajdistrelease {
+        # the epel packages change uid/gids + install paths between 5 & 6
+        5: {
+          $ganglia_uid = 298
+          $ganglia_gid = 298
+          $gmond_service_config = '/etc/gmond.conf'
+          $gmond_service_erb    = 'ganglia/gmond.conf.el5.erb'
 
-      $gmetad_service_config = '/etc/gmetad.conf'
-      $gmetad_service_erb    = 'ganglia/gmetad.conf.el5.erb'
+          $gmetad_service_config = '/etc/gmetad.conf'
+          $gmetad_service_erb    = 'ganglia/gmetad.conf.el5.erb'
+        }
+        # fedora is also part of $::osfamily = redhat so we shouldn't default
+        # to failing on el7.x +
+        6, default: {
+          $ganglia_uid = 211
+          $ganglia_gid = 211
+          $gmond_service_config = '/etc/ganglia/gmond.conf'
+          $gmond_service_erb    = 'ganglia/gmond.conf.el6.erb'
+
+          $gmetad_service_config = '/etc/ganglia/gmetad.conf'
+          $gmetad_service_erb    = 'ganglia/gmetad.conf.el6.erb'
+        }
+      }
     }
-    6: {
-      $ganglia_uid = 211
-      $ganglia_gid = 211
-      $gmond_service_config = '/etc/ganglia/gmond.conf'
-      $gmond_service_erb    = 'ganglia/gmond.conf.el6.erb'
-
-      $gmetad_service_config = '/etc/ganglia/gmetad.conf'
-      $gmetad_service_erb    = 'ganglia/gmetad.conf.el6.erb'
+    default: {
+      fail("Module ${module_name} is not supported on ${::operatingsystem}")
     }
   }
 }
