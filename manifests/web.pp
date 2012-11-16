69cd72050186242825853737523a4192a1455502
Joshua Hoblitt <jhoblitt@cpan.org>
2012-11-16 11:57:53 -0700

    add rdoc for ganglia::web

--- manifests/web.pp
+++ manifests/web.pp
@@ -1,3 +1,37 @@
+# == Class: ganglia::web
+#
+# install and configure the ganglia web front end
+#
+# === Parameters
+#
+# All parameteres are optional.
+#
+# [*ganglia_ip*]
+#   string - defaults to '127.0.0.1'
+#
+# [*ganglia_port*]
+#   string - defaults to '8652'
+#
+#
+# === Examples
+#
+#    class{ 'ganglia::web': }
+#
+#    class{ 'ganglia::web':
+#      $ganglia_ip = '192.168.0.1',
+#      $ganglia_port = 8652,
+#    }
+#
+#
+# === Authors
+#
+# Joshua Hoblitt <jhoblitt@cpan.org>
+#
+# === Copyright
+#
+# Copyright (C) 2012 Joshua Hoblitt
+#
+
 class ganglia::web(
   $ganglia_ip = '127.0.0.1',
   $ganglia_port = 8652,
