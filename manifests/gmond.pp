a762490cb36485e334262e1058ce62830d26f1f2
Joshua Hoblitt <jhoblitt@cpan.org>
2012-11-15 17:01:50 -0700

    add default params for all ganglia::gmond values
    Defaults are set per the stock values in gmond.conf from ganglia-gmond-3.1.7-6.el6.x86_64

--- manifests/gmond.pp
+++ manifests/gmond.pp
@@ -1,12 +1,16 @@
 class ganglia::gmond (
-  $cluster_name,
-  $cluster_owner,
-  $cluster_latlong,
-  $cluster_url,
-  $host_location,
-  $udp_recv_channel,
-  $udp_send_channel,
-  $tcp_accept_channel
+  $cluster_name       = 'unspecified',
+  $cluster_owner      = 'unspecified',
+  $cluster_latlong    = 'unspecified',
+  $cluster_url        = 'unspecified',
+  $host_location      = 'unspecified',
+  $udp_send_channel   = [
+    { mcast_join => '239.2.11.71', port => 8649, ttl => 1 }
+  ],
+  $udp_recv_channel   = [
+    { mcast_join => '239.2.11.71', port => 8649, bind => '239.2.11.71' }
+  ],
+  $tcp_accept_channel = [ { port => 8659 } ],
 ) inherits ganglia::params {
   validate_string($cluster_name)
   validate_string($cluster_owner)
