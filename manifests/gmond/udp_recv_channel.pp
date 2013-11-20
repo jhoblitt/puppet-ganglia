7bb6b64c9d5311d5a094c62e0da2be7bc899db39
Jeremy Kitchen <kitchen@kitchen.io>
2013-11-20 04:47:35 +0000

    some resourcification of gmond stanzas

---
+++ manifests/gmond/udp_recv_channel.pp
@@ -0,0 +1,27 @@
+# == Define: ganglia::gmond::udp_recv_channel
+#
+# configures a udp_recv_channel stanza for gmond
+#
+# === Authors
+#
+# Jeremy Kitchen <kitchen@kitchen.io>
+#
+# === Copyright
+#
+# Copyright (C) 2013 3DNA Corp
+#
+#
+define ganglia::gmond::udp_recv_channel (
+  $port,
+  $mcast_join = undef,
+  $bind       = undef,
+  $mcast_if   = undef,
+  $family     = undef,
+) {
+  include ::ganglia::params
+  
+  file { "${::ganglia::params::gmond_conf_d}/${name}.conf":
+    content => template('ganglia/gmond/udp_recv_channel.conf.erb'),
+    mode    => '0644',
+  }
+}
