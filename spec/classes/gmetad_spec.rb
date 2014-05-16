53988e96f77626b484758e78d7813103da60e5a3
Joshua Hoblitt <jhoblitt@cpan.org>
2014-05-16 14:47:16 -0700

    deeply validate ganglia::gmetad's clusters parameter
    This should address the confusing template error reported in #12 when
    incorrect data is passed to this parameter.

--- spec/classes/gmetad_spec.rb
+++ spec/classes/gmetad_spec.rb
@@ -3,19 +3,43 @@ require 'spec_helper'
 describe 'ganglia::gmetad' do
   let(:facts) {{ :osfamily => 'RedHat', :operatingsystemmajrelease => 6 }}
 
-  context 'with clusters' do
-    clusters = [
-      { 
-        'name'    => 'test', 
-        'address' => ['test1.example.org', 'test2.example.org'],
-      },
-    ]
+  context 'clusters =>' do
+    context '<good example>' do
+      clusters = [
+        { 
+          'name'    => 'test', 
+          'address' => ['test1.example.org', 'test2.example.org'],
+        },
+      ]
 
-    let(:params) { {:clusters => clusters} }
-    it do
-      should contain_class('ganglia::gmetad') 
-      should contain_file('/etc/ganglia/gmetad.conf') 
-    end
-  end
+      let(:params) {{ :clusters => clusters }}
+      it do
+        should contain_class('ganglia::gmetad') 
+        should contain_file('/etc/ganglia/gmetad.conf') 
+      end
+    end # <good example>
+
+    context '[]' do
+      let(:params) {{ :clusters => [] }}
+      it do
+        expect { should compile }.to raise_error(Puppet::Error, /Array may not be empty/)
+      end
+    end # <invalid example>
+
+    context '<invalid example>' do
+      clusters = [
+        { 
+          'name'    => 'test', 
+          'address' => ['test1.example.org', 'test2.example.org'],
+          'foo'     => 1,
+        },
+      ]
+
+      let(:params) {{ :clusters => clusters }}
+      it do
+        expect { should compile }.to raise_error(Puppet::Error, /unknown keys/)
+      end
+    end # <invalid example>
+  end # clusters =>
 end
 
