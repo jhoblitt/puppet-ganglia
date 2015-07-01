82f0d95bfa34f4d454049c71fc552472433d970c
Joshua Hoblitt <josh@hoblitt.com>
2015-07-01 15:43:40 -0700

    migrate & revise API docs into README
    API documentation was duplicated and inconsistent between the README and
    individual .pp files.  This change makes the README unambiguously
    authoritative.

--- manifests/gmetad.pp
+++ manifests/gmetad.pp
@@ -1,59 +1,5 @@
 # == Class: ganglia::gmetad
 #
-# install and configure the ganglia gmetad daemon
-#
-# === Parameters
-#
-# All parameteres are optional.
-#
-# [*all_trusted*]
-#   boolean, default false; if set to true, we will allow all hosts that
-#   can query ganglia data via the xml query port.  Corresponds to the
-#   'all_trusted' field in gmetad.conf.
-#
-# [*clusters*]
-#   array of hashes.  Valid keys are:
-#
-#   -name
-#   -polling_interval
-#   -address
-#
-#   defaults to:
-#   [ { name => 'my cluster', address => 'localhost' } ],
-#
-# [*gridname*]
-#   string - defaults to '', which means no gridname at all
-#
-# [*trusted_hosts*]
-#   array of strings, each of which matches a hostname that is allowed to
-#   query ganglia data via the xml query port.  Corresponds to the
-#   'trusted_hosts' field in gmetad.conf.
-#
-# === Examples
-#
-#
-#    $clusters = [
-#      {
-#        name     => 'test',
-#        address  => ['test1.example.org', 'test2.example.org'],
-#      },
-#    ]
-#
-#    class{ 'ganglia::gmetad':
-#      clusters => $clusters,
-#      gridname => 'my grid',
-#    }
-#
-#
-# === Authors
-#
-# Joshua Hoblitt <jhoblitt@cpan.org>
-#
-# === Copyright
-#
-# Copyright (C) 2012-2013 Joshua Hoblitt
-#
-
 class ganglia::gmetad(
   $all_trusted                     = false,
   $clusters                        = [
--- manifests/web.pp
+++ manifests/web.pp
@@ -1,37 +1,5 @@
 # == Class: ganglia::web
 #
-# install and configure the ganglia web front end
-#
-# === Parameters
-#
-# All parameteres are optional.
-#
-# [*ganglia_ip*]
-#   string - defaults to '127.0.0.1'
-#
-# [*ganglia_port*]
-#   string - defaults to '8652'
-#
-#
-# === Examples
-#
-#    class{ 'ganglia::web': }
-#
-#    class{ 'ganglia::web':
-#      $ganglia_ip = '192.168.0.1',
-#      $ganglia_port = 8652,
-#    }
-#
-#
-# === Authors
-#
-# Joshua Hoblitt <jhoblitt@cpan.org>
-#
-# === Copyright
-#
-# Copyright (C) 2012-2013 Joshua Hoblitt
-#
-
 class ganglia::web(
   $ganglia_ip   = '127.0.0.1',
   $ganglia_port = 8652,
