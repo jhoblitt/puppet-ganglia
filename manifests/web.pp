db8498244a9b62b892c7fc74e0a78839b596a714
Joshua Hoblitt <josh@hoblitt.com>
2015-07-01 12:49:53 -0700

    deprecate passing a String the ganglia::web param ganglia_port
        Passing a String to $ganglia_port is deprecated.  Please use an Integer
        value.

--- manifests/web.pp
+++ manifests/web.pp
@@ -33,12 +33,15 @@
 #
 
 class ganglia::web(
-  $ganglia_ip = '127.0.0.1',
+  $ganglia_ip   = '127.0.0.1',
   $ganglia_port = 8652,
 ) inherits ganglia::params {
   validate_string($ganglia_ip)
   if !(is_string($ganglia_port) or is_integer($ganglia_port)) {
     fail('$ganglia_port is not a string or integer.')
+  }
+  if (is_string($ganglia_port)) {
+    warning('Passing a String to $ganglia_port is deprecated.  Please use an Integer value.')
   }
 
   if versioncmp($::puppetversion, '3.6.0') > 0 {
--- spec/classes/web_spec.rb
+++ spec/classes/web_spec.rb
@@ -3,11 +3,37 @@ require 'spec_helper'
 describe 'ganglia::web' do
   let(:facts) {{ :osfamily => 'RedHat', :operatingsystemmajrelease => '6' }}
 
-  context 'with out params' do
-    let(:params) { { } }
-    it do
-      should contain_class('ganglia::web')
-      should contain_file('/etc/ganglia/conf.php')
+  context 'parameters' do
+    context 'with out params' do
+      it do
+        should contain_file('/etc/ganglia/conf.php').
+          with_content(/\$ganglia_port = 8652;/)
+      end
     end
-  end
+
+    context 'ganglia_port' do
+      context 'Integer' do
+        let(:params) {{ :ganglia_port => 42 }}
+
+        it do
+          should contain_file('/etc/ganglia/conf.php').
+            with_content(/\$ganglia_port = 42;/)
+        end
+      end #Integer
+
+      context 'String' do
+        let(:params) {{ :ganglia_port => '42' }}
+
+        it do
+          should contain_file('/etc/ganglia/conf.php').
+            with_content(/\$ganglia_port = 42;/)
+        end
+
+        it 'should be deprecated' do
+          pending 'https://github.com/rodjek/rspec-puppet/issues/310'
+          scope.expects(:warning).with('Passing a String to $ganglia_port is deprecated.  Please use an Integer value.')
+        end
+      end #Integer
+    end # ganglia_port
+  end # parameters
 end
